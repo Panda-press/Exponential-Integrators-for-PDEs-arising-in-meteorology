8980b6205b8e58b83d3a98153a42f82eef3ece45
Harvey <tyrrellhs@gmail.com>
2024-09-23 17:01:28 +0100

    Plotting Results

--- FETester.py
+++ FETester.py
@@ -5,6 +5,7 @@ import hashlib
 import copy
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 from ufl import *
 from dune.ufl import Space, Constant
 from dune.fem import integrate
@@ -16,8 +17,8 @@ from dune.alugrid import aluCubeGrid as leafGridView
 from dune.fem.view import adaptiveLeafGridView as view
 from steppers import BEStepper, steppersDict
 
-# results_folder = "FETests"
-results_folder = "FETests2009"
+results_folder = "FETests"
+# results_folder = "FETests2009"
 
 class Tester():
     def __init__(self, 
@@ -26,6 +27,7 @@ class Tester():
                  problem_name, 
                  seed_time = 0, 
                  setup_tau = 1e-2,
+                 target_tau = 1e-4,
                  setup_stepper = BEStepper,
                  exact = None,
                  **stepper_args):
@@ -35,6 +37,7 @@ class Tester():
         self.initial_condition = initial_condition
         self.seed_time = seed_time
         self.setup_stepper = setup_stepper(op, **stepper_args)
+        self.target_tau = target_tau
         self.folder = results_folder + "/Problem:{0}_Grid_Size:{4}_Setup_Tau:{2}_Setup_Stepper:{1}_Seed_Time:{3}".format(problem_name,self.setup_stepper.name,setup_tau,self.seed_time,self.N)
         self.exact = exact
         
@@ -78,9 +81,9 @@ class Tester():
 
         # Generate target data if it doesn't exist
         if self.exact is None:
-            target_file_name = self.get_target_results_file(tau, end_time)
+            target_file_name = self.get_target_results_file(self.target_tau, end_time)
             if not os.path.isfile(target_file_name):
-                self.target,self.target_countN = self.run(tau, self.setup_stepper, self.initial_condition, self.seed_time, end_time)
+                self.target,self.target_countN = self.run(self.target_tau, self.setup_stepper, self.initial_condition, self.seed_time, end_time)
                 with open(target_file_name, 'wb') as file:
                     pickle.dump([self.target.as_numpy[:],self.target_countN], file)
             else:
@@ -95,7 +98,10 @@ class Tester():
         # Generate test stepper data if it doesn't exist
         test_stepper = test_stepper(op, **stepper_args)
         temp = copy.deepcopy(stepper_args)
-        temp['exp_v'][0] = 0
+        try:
+            temp['exp_v'][0] = 0
+        except:
+            pass
         test_stepper_name = test_stepper.name +\
                             hashlib.sha1( repr(sorted(temp.items())).encode('utf-8') ).hexdigest()
         test_file_name = self.get_test_results_file(tau, test_stepper_name, end_time)
@@ -129,26 +135,28 @@ if __name__ == "__main__":
         from allenCahn import dimR, time, sourceTime, domain
         from allenCahn import test2 as problem
         problemName = "Allen Cahn Test2"
-        start_time = 1
-        end_time = 3
+        start_time = 4
+        end_time = 6
+        tau0 = 1e-1
     else:
         from parabolicTest import dimR, time, sourceTime, domain
         from parabolicTest import paraTest1 as problem
         problemName = "Parabolic Test1"
         tau0 = 1e-4  # 2e-3,N:32,Tau:0.002: compare m=5->m=10
-        start_time = 0
+        start_time = 0.0005
         end_time = 0.001
+    exp_methods = ["EXPARN", "EXPLAN", "EXPKIOPS", "BE"]
 
     results = []
 
 
-    exp_methods = ["EXPARN", "EXPLAN", "EXPKIOPS"]
     # exp_methods = ["EXPLAN", "EXPARN", "EXPKIOPS"]
+
 
     domain = list(domain)
     for exp_method in exp_methods:
         print("EXP method:{0}".format(exp_method))
-        for N in [2**i for i in range(5, 8)]:
+        for N in [10, 30, 60, 120]:
             print("N:{0}".format(N))
             for tau in tau0*np.array([1/2**i for i in range(0, 4)]):
                 print("Tau:{0}".format(tau))
@@ -173,8 +181,8 @@ if __name__ == "__main__":
                 
                 tester.produce_results(tau, exp_stepper, args, end_time)
 
-                tester.test_results.plot()
-                tester.target.plot()
+                #tester.test_results.plot()
+                #tester.target.plot()
                 error = tester.test_results - tester.target
                 ref = [ np.sqrt(r) for r in
                         integrate([tester.target**2,
@@ -194,24 +202,65 @@ if __name__ == "__main__":
     # produce plots using 'results'
 
     results = pd.DataFrame(results)
-    results.columns = ["Method", "Grid size", "Tau", "Error L2", "Error H1", "Target N Count", "Test N Count"]
+    results.columns = ["Method", "Grid Size", "Tau", "Error L2", "Error H1", "Target N Count", "Test N Count"]
 
+    # N count vs tau
+    markers = ["1", "2", "3", "4"]
+    for grid_size in results["Grid Size"].unique():
+        if "BE" not in exp_methods:
+            plt.scatter(results["Tau"], results["Target N Count"], marker="x", label="BE Method")
+        for i, exp_method in enumerate(exp_methods):
+            trimmed_data = results[results["Method"] == exp_method]
+            trimmed_data = trimmed_data[trimmed_data["Grid Size"] == grid_size]
 
-    plt.figure(1)
-    plt.scatter(results["Tau"], results["Target N Count"], marker=".", label="BE Method")
+            plt.scatter(trimmed_data["Tau"], trimmed_data["Test N Count"], marker=markers[i], label=exp_method)
+
+        plt.legend()
+        plt.xlabel("Tau")
+        plt.ylabel("Calls")
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.grid(True, which="both")
+        plt.title(f"Tau V Opperator Calls for {problemName} with grid size {grid_size}")
+        plt.savefig(f"FEMethodPlots/Tau V Operator Calls for {problemName} with grid size {grid_size}.png")
+        plt.close()
+
+    # N count v Error
+    markers = ["1", "2", "3", "4"]
+    for grid_size in results["Grid Size"].unique():
+        if "BE" not in exp_methods:
+            plt.scatter(results["Target N Count"], results["Error L2"], marker="x", label="BE Method")
+        for i, exp_method in enumerate(exp_methods):
+            trimmed_data = results[results["Method"] == exp_method]
+            trimmed_data = trimmed_data[trimmed_data["Grid Size"] == grid_size]
+
+            plt.scatter(trimmed_data["Test N Count"], trimmed_data["Error L2"], marker=markers[i], label=exp_method)
+
+        plt.legend()
+        plt.xlabel("Calls")
+        plt.ylabel("L2 Error")
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.grid(True, which="both")
+        plt.title(f"Opperator Calls V L2 Error for {problemName} with grid size {grid_size}")
+        plt.savefig(f"FEMethodPlots/Operator Calls V Error for {problemName} with grid size {grid_size}.png")
+        plt.close()
+
+    # Tau vs error
     for exp_method in exp_methods:
         trimmed_data = results[results["Method"] == exp_method]
-        plt.figure(1)
-        plt.scatter(trimmed_data["Tau"], trimmed_data["Test N Count"], marker=".", label=exp_method)
-        plt.figure(2)
-        plt.scatter(trimmed_data["Tau"], trimmed_data["Error L2"], marker=".", label=exp_method)
-        print(trimmed_data["Tau"],trimmed_data["Error L2"])
+        for grid_size in results["Grid Size"].unique():
+            grid_data = trimmed_data[trimmed_data["Grid Size"] == grid_size]
 
-    plt.figure(1)
-    plt.legend()
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.figure(2)
-    plt.legend()
-    plt.xscale('log')
-    plt.show()
+            plt.plot(grid_data["Tau"], grid_data["Error L2"], marker=".", label=f"Grid Size {grid_size}")
+        plt.title(f"Tau V L2 Error for {exp_method} for {problemName}")
+        plt.xlabel("Tau")
+        plt.ylabel("L2 Error")
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.legend()
+        plt.grid(True, which="both")
+        plt.savefig(f"FEMethodPlots/Tau V L2 Error for {exp_method} for {problemName}.png")
+        plt.close()
+
+
